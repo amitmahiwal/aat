63f31e3f9ef56a65fdaedd829d060112794e1ef4
Tim Paine <t.paine154@gmail.com>
2020-03-15 22:49:19 -0400

    .

--- aat/__main__.py
+++ aat/__main__.py
@@ -1,5 +1,18 @@
 import sys
-from .main import main
+from .trading import TradingEngine
+
+
+def main(argv: list) -> None:
+    # Instantiate trading engine
+    #
+    # The engine is responsible for managing the different components,
+    # including the strategies, the bank/risk engine, and the
+    # exchange/backtest engine.
+    engine = TradingEngine()
+
+    # Run the live trading engine
+    engine.run()
+
 
 if __name__ == '__main__':
     main(sys.argv)
